d868f64acc7272e643473d7a411a9318c8defc4e
Weronika Tomaszewska <vikariusu@gmail.com>
2020-02-28 14:30:22 -0500

    feat: change bookshelf on input change (#7)

--- src/BookItem.js
+++ src/BookItem.js
@@ -1,24 +1,33 @@
 import React from 'react';
 
-export default function BookItem({ book }) {
-    return (
-        <li>
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                    <div className="book-shelf-changer">
-                        <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
+export default class BookItem extends React.Component {
+    onChangeSelect = (newShelf) => {
+        this.props.onShelfChange(this.props.book, newShelf);
+    }
+
+    render() {
+        const { book } = this.props;
+
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                            <select value={this.props.book.shelf} onChange={(e) => this.onChangeSelect(e.target.value)}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
                     </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors.join(', ')}</div>
                 </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors.join(', ')}</div>
-            </div>
-        </li>
-    )
+            </li>
+        )
+    }
+
 }
--- src/BookList.js
+++ src/BookList.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import BookItem from './BookItem';
 
-export default function BookList({ name, books }) {
+export default function BookList({ name, books, onShelfChange }) {
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{name}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {books.map(book => (
-                        <BookItem book={book} />
-                    ))}
+                    {books.length === 0 ? <div>There are no books in this category</div>
+                        : books.map(book => (
+                            <BookItem book={book} onShelfChange={onShelfChange} key={book.id} />
+                        ))}
                 </ol>
             </div>
         </div>
