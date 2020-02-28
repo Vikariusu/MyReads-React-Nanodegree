b488d8dfd50c0c8aaa9390e9ed4cf2b12ac7a1e1
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-02-28 14:29:53 -0500

    feat: change bookshelf on input change

--- src/App.js
+++ src/App.js
@@ -12,12 +12,23 @@ class BooksApp extends React.Component {
   }
 
   componentDidMount = () => {
+    this.fetchAllBooks()
+  }
+
+  fetchAllBooks = () => {
     BooksAPI.getAll()
       .then(books => this.setState({ books }))
   }
 
   filterBooks = (shelfName) => {
     return this.state.books.filter(book => book.shelf === shelfName)
+  }
+
+  // move book to a different shelf
+  onShelfChange = async (book, shelfName) => {
+    await BooksAPI.update(book, shelfName)
+
+    this.fetchAllBooks();
   }
 
   render() {
@@ -34,9 +45,19 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <BookList name="Currently Reading" books={this.filterBooks('currentlyReading')} />
-                <BookList name="Want to Read" books={this.filterBooks('wantToRead')} />
-                <BookList name="Read" books={this.filterBooks('read')} />
+                <BookList
+                  name="Currently Reading"
+                  books={this.filterBooks('currentlyReading')}
+                  onShelfChange={this.onShelfChange}
+                />
+                <BookList name="Want to Read"
+                  books={this.filterBooks('wantToRead')}
+                  onShelfChange={this.onShelfChange} />
+                <BookList
+                  name="Read"
+                  books={this.filterBooks('read')}
+                  onShelfChange={this.onShelfChange}
+                />
               </div>
             </div>
             <div className="open-search">
